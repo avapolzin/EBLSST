d8b91758cdc3746dfc22c97eb0a31cf4c3460ce5
Aaron Geller <ageller@williamson.local>
2018-07-10 20:53:05 -0500

    updated directories in MPIdriver

--- code/MPIdriver.py
+++ code/MPIdriver.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
 
 	#Our LSST EB class to use gatspy and ellc
 	worker = LSSTEBworker()
-	worker.filterFilesRoot = '/projects/p30137/ageller/input/filters/'
+	worker.filterFilesRoot = '/projects/p30137/ageller/EBLSST/input/filters/'
 
 	#check for command-line arguments
 	apply_args(worker, args)	
