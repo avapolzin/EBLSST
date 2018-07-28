fixed magnitude calculation

code/EBLSST.py
@@ -1,8 +1,5 @@
-## Have Adam double check the conversion from bolometric to apparent magnitude
 ## ellc.lc is in arbitrary flux units... am I using this correctly?
 
-#try this for LSST mags?
-# http://sncosmo.readthedocs.io/en/v1.5.x/models.html 
 
 import math
 import scipy.special as ss
@@ -25,6 +22,15 @@ import sqlite3
 import astropy.stats as astroStats
 from astropy import units, constants
 from astropy.coordinates import SkyCoord, ICRS
+from astropy.io import fits
+
+#pysynphot from STScI
+#https://pysynphot.readthedocs.io/en/latest/index.html#pysynphot-installation-setup
+#need to download grid of models manually, and set path accordingly
+import pysynphot as pyS
+# import os
+# #export PYSYN_CDBS=/my/local/dir/cdbs/
+# print(os.environ['PYSYN_CDBS'])
 
 #3rd party codes
 import ellc
@@ -67,6 +73,12 @@ class EclipsingBinary(object):
 		self.lineNum = 0
 		self.verbose = False
 		self.RV = 3.1
+		self.M_H = 0. #metallicity
+
+		#for SED
+		self.filterFilesRoot = '../input/filters/'
+		self.SED1 = None
+		self.SED2 = None
 
 		#from https://www.lsst.org/scientists/keynumbers
 		#in nm
@@ -110,7 +122,6 @@ class EclipsingBinary(object):
 		#for light curves
 		self.SED = None
 		self.filters = filters
-		self.M_H = 0
 		self.ld_1 = 'claret'
 		self.ld_2 = 'claret'
 		self.grid_1 = 'default'
@@ -331,7 +342,7 @@ class EclipsingBinary(object):
 			# self.appMag[filt] = absMag + 5.*np.log10(self.dist*100.) + self.Ared[filt]  #multiplying by 1000 to get to parsec units
 
 			Fv = self.Fv1[filt] + self.Fv2[filt]
-			self.appMag[filt] = -2.5*np.log10(Fv*lc/(3631.*units.Jansky)) + self.Ared[filt] #AB magnitude 
+			self.appMag[filt] = -2.5*np.log10(Fv*lc) + self.Ared[filt] #AB magnitude 
 
 			# plt.plot((self.obsDates[filt] % self.period), lc,'.')
 			# plt.ylim(min(lc), max(lc))
@@ -444,6 +455,26 @@ class EclipsingBinary(object):
 		self.R_1e = self.r1/self.Eggleton_RL(self.m1/self.m2, self.a * (1. - self.eccentricity))
 		self.R_2e = self.r2/self.Eggleton_RL(self.m2/self.m1, self.a * (1. - self.eccentricity))
 
+		self.SED1 = SED()
+		self.SED1.filterFilesRoot = self.filterFilesRoot
+		self.SED1.T = self.T1*units.K
+		self.SED1.R = self.r1*units.solRad
+		self.SED1.L = self.L1*units.solLum
+		self.SED1.logg = self.g1
+		self.SED1.M_H = self.M_H
+		self.SED1.EBV = self.AV/self.RV #could use this to account for reddening in SED
+		self.SED1.initialize()
+
+		self.SED2 = SED()
+		self.SED2.filterFilesRoot = self.filterFilesRoot
+		self.SED2.T = self.T2*units.K
+		self.SED2.R = self.r2*units.solRad
+		self.SED2.L = self.L2*units.solLum
+		self.SED2.logg = self.g2
+		self.SED2.M_H = self.M_H
+		self.SED2.EBV = self.AV/self.RV #could use this to account for reddening in SED
+		self.SED2.initialize()
+
 		#estimate a combined Teff value, as I do in the N-body codes (but where does this comes from?)
 		logLb = np.log10(self.L1 + self.L2)
 		logRb = 0.5*np.log10(self.r1**2. + self.r2**2.)
@@ -461,16 +492,25 @@ class EclipsingBinary(object):
 		#one option for getting the extinction
 		if (self.AV == None):
 			self.AV = vespa.stars.extinction.get_AV_infinity(self.RA, self.Dec, frame='icrs')
+		#ext = F04(Rv=self.RV)
 		ext = F04(Rv=self.RV)
+		#a check
+		# self.Ltest = self.SED.getL(self.T1*units.K, self.r1*units.solRad)
+		#definitely necessary for Kurucz because these models are not normalized
+		#for the bb  I'm getting a factor of 2 difference for some reason
+		Lconst1 = self.SED1.getLconst()
+		Lconst2 = self.SED2.getLconst()
+		#print(np.log10(Lconst1), np.log10(Lconst2))
 		for f in self.filters:
 			#print(extinction.fitzpatrick99(np.array([self.wavelength[f]*10.]), self.AV, self.RV, unit='aa')[0] , ext(self.wavelength[f]*units.nm)*self.AV)
 			#self.Ared[f] = extinction.fitzpatrick99(np.array([self.wavelength[f]*10.]), self.AV, self.RV, unit='aa')[0] #or ccm89
 			self.Ared[f] = ext(self.wavelength[f]*units.nm)*self.AV
 
-			self.Fv1[f] = self.SED.getFv(self.L1*units.solLum, self.T1*units.K, self.r1*units.solRad, self.dist*units.kpc, f)
-			self.Fv2[f] = self.SED.getFv(self.L2*units.solLum, self.T2*units.K, self.r2*units.solRad, self.dist*units.kpc, f)
+
+			self.Fv1[f] = self.SED1.getFvAB(self.dist*units.kpc, f, Lconst = Lconst1)
+			self.Fv2[f] = self.SED2.getFvAB(self.dist*units.kpc, f, Lconst = Lconst2)
 			Fv = self.Fv1[f] + self.Fv2[f]
-			self.appMagMean[f] = -2.5*np.log10(Fv/(3631.*units.Jansky)) + self.Ared[f] #AB magnitude 
+			self.appMagMean[f] = -2.5*np.log10(Fv) + self.Ared[f] #AB magnitude 
 
 			#print(self.wavelength[f], self.appMagMean[f], self.Ared[f], self.T1)
 
@@ -888,10 +928,21 @@ class SED(object):
 		self.filters = filters
 		self.filterThroughput = dict()
 
-		self.BCf = dict()
+		self.useSpecModel = True
+		self.specModelName = None
+		self.specModel = None
+		self.extinctionModel = None
+
+		self.T = None
+		self.R = None
+		self.L = None
+		self.logg = None
+		self.M_H = None
+		self.EBV = None
 
 	def readFilters(self):
 		for f in self.filters:
+			#these are nearly identical to what is below, but the others seem like they're in a more obvious location online
 			# #https://github.com/lsst-pst/syseng_throughputs/tree/master/components/camera/filters
 			# fname = self.filterFilesRoot + f + 'band_Response.dat'
 			# df = pd.read_csv(fname, delim_whitespace=True, header=None, names=['w','t'])
@@ -908,70 +959,133 @@ class SED(object):
 			df.sort_values(by='nu', inplace=True)
 			self.filterThroughput[f] = {'nu':df['nu'].values, 't':df['t'].values, 'numin':numin, 'numax':numax, 'wmin':wmin, 'wmax':wmax}
 
-	def bb(self, w, T):
+	# def samplespecModel(self, nu): 
+	# 	w = (constants.c/(nu*units.Hz)).to(units.angstrom).value
+	# 	fac = ((w*units.angstrom)**2. / constants.c).value #not sure what to do about units, want to return Fnu
+	# 	fK = np.interp(w, self.specModel.wave, self.specModel.flux)
+	# 	print(fK, fac, w)
+	# 	return fK*fac
+
+	def intspec(self):
+		nu = self.specModel.nu
+		dnu = np.ediff1d(nu)*-1. #because it goes in the oposite direction
+		fnu = np.sum(self.specModel.fnu[:-1] *dnu)
+		return fnu
+
+	def intspecFilter(self, filt):
+		nu = self.specModel.nu
+		w = (constants.c/(nu*units.Hz)).to(units.angstrom).value
+		dnu = np.ediff1d(nu)*-1. #because it goes in the oposite direction
+		ft = np.interp(nu, self.filterThroughput[filt]['nu'], self.filterThroughput[filt]['t'])
+		#ext = self.extinctionModel(w)
+		#ffnu = np.sum(self.specModel.fnu[:-1] * ft[:-1] * ext[:-1] * dnu)
+		ffnu = np.sum(self.specModel.fnu[:-1] * ft[:-1] * dnu)
+		return ffnu
+
+
+	def bb(self, w):
 		#in cgs is the Ba / s
 		#expects w in nm but without a unit attached
 		w *= units.nm
-		Bl = 2.*constants.h*constants.c**2./w**5. / (np.exp( constants.h*constants.c / (w*constants.k_B*T)) -1.)
+		Bl = 2.*constants.h*constants.c**2./w**5. / (np.exp( constants.h*constants.c / (w*constants.k_B*self.T)) -1.)
 		return Bl.cgs.value
 
-
-	#this return inf when integrated!
-	def bbv(self, nu, T):
+	#this return inf when integrated to infs	
+	def bbv(self, nu):
 		#expects nu in 1/s, but without a unit attached
 		#return in g/s**2
 		nu *= units.Hz
-		Bl = 2.*constants.h/constants.c**2. *nu**3. / (np.exp( constants.h*nu / (constants.k_B*T)) -1.)
+		Bl = 2.*constants.h/constants.c**2. *nu**3. / (np.exp( constants.h*nu / (constants.k_B*self.T)) -1.)
 		return Bl.cgs.value
 
 	def filter(self, nu, filt):
 		ft = np.interp(nu, self.filterThroughput[filt]['nu'], self.filterThroughput[filt]['t'])
 		return ft
 
-	def bbvFilter(self, nu, T, filt):
-		fB = self.bbv(nu,T)
+	def bbvFilter(self, nu, filt):
+		fB = self.bbv(nu)
 		ft = self.filter(nu, filt)
 		return fB*ft
 
-	def getFv(self, L, T, R, dist, filt, dw = 1e-4):
+	def getL(self):
+		if (self.useSpecModel):
+			fB = self.intspec()
+			fB *= units.g/units.s**2.*units.Hz
+		else:
+			#integrating this over nu returns infs??!!
+			fB, fB_err = quad(self.bb, 0, np.inf, limit=1000)
+			fB *= units.Ba/units.s*units.nm
+		LB = 4.*np.pi*self.R**2. * fB 
+
+		#print("LB = ", LB.to(units.solLum))
+		return LB.to(units.solLum)
+
+	def getLconst(self):
+		#to account for differences between blackbody and true stellar atmosphere, if true (bolometric) luminosity is known (seems unecessary)
+		LB = self.getL()
+		return self.L/LB
+
+	def getFvAB(self, dist, filt, Lconst = 1.):
 		#http://burro.case.edu/Academics/Astr221/Light/blackbody.html
 		#F = sigma*T**4
 		#L = 4*pi**2 * R**2 * F
 		#Lv*dv = 4*pi**2 * R**2 * Bv*dv
 		#Fv*dv = Bv*dv
 
-		# def getConst(L, T):
-		# 	#integrating this over nu returns infs??!!
-		# 	fB, fB_err = quad(self.bb, 0, np.inf, args= (T,))
-		# 	fB *= units.Ba/units.s*units.nm
-		# 	# LB = 4.*np.pi**2.*R**2. * fB
-		# 	# print("L,LB = ",L, LB.to(units.solLum))
-		# 	A = (L/fB).decompose()
-		# 	return A
-
-		# A = getConst(L,T)
-		#print("A=",A)
 
 		#quad misses the filter, and just returns zero when given np.inf limits! and is slow.  So I will do the summation with a small dw
-		w = np.arange(self.filterThroughput[filt]['wmin'], self.filterThroughput[filt]['wmax'], dw)
-		nu = (constants.c/(w*units.nm)).to(units.Hz).value
-		# dnu = (constants.c/(w*units.nm)**2.*(dw*units.nm)).to(units.Hz).value
-		# f = np.sum(self.filter(nu,filt)*dnu) *units.Hz
-		# fBf = np.sum(self.bbvFilter(nu,T,filt)*dnu) *units.g/units.s**2 *units.Hz
-		f = np.sum(self.filter(nu,filt))
-		fBf = np.sum(self.bbvFilter(nu,T,filt)) *units.g/units.s**2 	
 
-		# fBv = fBf/f * A/(4.*np.pi**2.*dist**2.) 
+		#dnu = (constants.c/(w*units.nm)**2.*(dw*units.nm)).to(units.Hz).value
+		if (self.useSpecModel):
+			nu = self.specModel.nu
+			dnu = np.ediff1d(nu)*-1. #because it goes in the oposite direction
+			fBf = self.intspecFilter(filt) *units.g/units.s**2  * units.Hz 
+		else:
+			dw = 1e-4
+			w = np.arange(self.filterThroughput[filt]['wmin'], self.filterThroughput[filt]['wmax'], dw)
+			nu = (constants.c/(w*units.nm)).to(units.Hz).value
+			dnu = np.ediff1d(nu)*-1. #because it goes in the oposite direction
+			fBf = np.sum(self.bbvFilter(nu,filt)[:-1]*dnu) *units.g/units.s**2 *units.Hz
+		f = np.sum(3631.*units.Jansky*self.filter(nu,filt)[:-1]*dnu ) * units.Hz #AB magnitude zero point
 
-		fBv = fBf/f * R**2./dist**2. 
+		# #the dnu will divide out anyway
+		# f = np.sum(self.filter(nu,filt))
+		# fBf = np.sum(self.bbvFilter(nu,filt)) *units.g/units.s**2 	
 
-		# print(f, fBf.to(units.Jansky), fBv.to(units.Jansky))
+
+		# fBv = fBf/f * A/(4.*np.pi*dist**2.) 
+
+		# print("T, Lconst", T, self.Lconst)
+		fBv = fBf/f * self.R**2./dist**2. * Lconst
+
+		#print(f, fBf, fBv)
 
 		# mAB = -2.5*np.log10(fBv/(3631.*units.Jansky))
 		# print("mAB =", mAB)
 
 		return fBv
 
+	def initialize(self):
+		self.readFilters()
+		if (self.useSpecModel):
+			# if (self.T > 3500*units.K):
+			#for Kurucz
+			self.specModelName = 'ck04models'
+			g = np.clip(self.logg, 3, 5.)
+			T = np.clip(self.T.to(units.K).value, 3500., 50000.0)
+			MH = np.clip(self.M_H, -4, 1) #not sure what the limits are here, but this seems reasonable
+			# else:
+			# 	#phoenix for cooler stars, but appear to be giving more discrepant results than just using the Kurucz model
+			# 	self.specModelName = 'phoenix'
+			# 	g = np.clip(self.logg, 0, 4.5)
+			# 	T = np.clip(self.T.to(units.K).value, 2000., 7000.0)
+			# 	MH = np.clip(self.M_H, -4, 1)
+			#print("parameters", self.logg,g, self.T.to(units.K).value,T, self.M_H, MH)
+			self.specModel = pyS.Icat(self.specModelName, T, MH, g)
+			self.specModel.nu = (constants.c/(self.specModel.wave*units.angstrom)).to(units.Hz).value
+			self.specModel.fnu = ((((self.specModel.wave*units.angstrom)**2./constants.c) * (self.specModel.flux*units.Ba/units.s)).to(units.g/units.s**2.)).value
+		#not using this (gives essentially the same values as above, but using an outdated reddening law)
+		#self.extinctionModel = pyS.Extinction(self.EBV, 'gal1') #This seems to be a work in progress on their end, I can only access gal1, which is deprecated
 
 
 ###########################################################################################################
@@ -1032,7 +1146,6 @@ class LSSTEBworker(object):
 
 		self.seed = None
 
-		self.SED = None
 
 	#database manipulation
 	def getCursors(self):
@@ -1206,7 +1319,7 @@ class LSSTEBworker(object):
 
 		# EB.seed = self.seed + i
 		EB.initializeSeed()
-		EB.SED = self.SED
+		EB.filterFilesRoot = self.filterFilesRoot
 
 		#solar units
 		EB.m1 = line[0]
@@ -1232,6 +1345,8 @@ class LSSTEBworker(object):
 
 		if (len(line) >= 15):
 			EB.AV = line[14]
+		if (len(line) >= 16):
+			EB.M_H = line[15]
 
 		EB.t_zero = np.random.random() * EB.period
 
@@ -1276,7 +1391,4 @@ class LSSTEBworker(object):
 		else:
 			np.random.seed(seed = self.seed)
 
-		self.SED = SED()
-		self.SED.filterFilesRoot = self.filterFilesRoot
-		self.SED.readFilters()
 